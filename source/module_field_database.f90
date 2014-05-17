f86259860424a538dfd910816621ede735594880
fran-pena <fran.pena@gmail.com>
2014-05-17 18:51:55 +0200

    Clean field_database

--- source/module_field_database.f90
+++ source/module_field_database.f90
@@ -41,17 +41,3 @@ enddo
 end function
 
 end module
-
-!Innecesary/old constants:
-!Element types
-!integer, parameter :: NODE2 = 1, NODE3 = 2, &                                   !nodes
-!ED2_P1 =  3, ED3_P1 =  4, ED2_P2 =  5, ED3_P2 =  6, &                           !edges 
-!TR2_P1 =  7, TR3_P1 =  8, TR2_P2 =  8, TR3_P2 = 10, TR2_RT = 11, TR3_RT = 12, & !triangles
-!QU2_P1 = 13, QU3_P1 = 14, QU2_P2 = 15, QU3_P2 = 16, QU2_RT = 17, QU3_RT = 18, & !quadrangles
-!TET_P1 = 19, TET_P2 = 20, TET_RT = 21, TET_ND = 22, &                           !tetrahedra
-!HEX_P1 = 23, HEX_P2 = 24, HEX_RT = 25, HEX_ND = 26                              !hexahedra
-!EDGE_WEDGE(i,j), vertex #i of edge #j of a wedge
-!integer, parameter :: EDGE_WEDGE(2,9) = reshape([1,2, 2,3, 3,1, 1,4, 2,5, 3,6, 4,5, 5,6, 6,4], [2,9])
-!FACE_WEDGE(i,j), vertex #i of face #j of a wedge; note that faces 1 and 4 have only three valid vertices
-!integer, parameter :: FACE_WEDGE(4,5) = reshape([1,3,2,0, 1,4,6,3, 1,2,5,4, 4,5,6,0, 2,3,6,5], [4,5]) 
-
